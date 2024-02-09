4f6c2cc702021fc00553b1d39ddcfd2048258e96
Hyn2 <justin010129@gmail.com>
2024-02-09 14:13:53 +0900

    Migration: Userテーブルスキーマ変更

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('phone_number', 20)->nullable();
             $table->string('email', 50)->unique();
             $table->string('password')->nullable();
+            $table->boolean('approved')->default(false);
             $table->timestamps();
         });
     }
