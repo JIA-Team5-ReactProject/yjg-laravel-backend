6f905678f474f4318198e3e622f760fb3bce95a4
Hyn2 <justin010129@gmail.com>
2024-02-12 22:28:16 +0900

    Feat: 美容室予約関連機能実装

--- app/Http/Controllers/Admin/AdminSalonReservationController.php
+++ app/Http/Controllers/Admin/AdminSalonReservationController.php
@@ -10,11 +10,85 @@ use Illuminate\Validation\ValidationException;
 
 class AdminSalonReservationController extends Controller
 {
-    public function index(Request $request)
+    /**
+     * @OA\Get (
+     *     path="/api/admin/salon-reservation",
+     *     tags={"미용실"},
+     *     summary="예약 검색",
+     *     description="미용실 예약 검색",
+     *     @OA\RequestBody(
+     *         description="검색할 옵션",
+     *         required=true,
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema (
+     *                 @OA\Property (property="status", type="char", description="예약의 상태(SCR 중 하나로 보내면 됨, 각각 Submit, Confirm, Reject 임)", example="S"),
+     *                 @OA\Property (property="start_date", type="date", description="검색 시작일", example="2001-01-29"),
+     *                 @OA\Property (property="end_date", type="date", description="검색 종료일", example="2023-02-12"),
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(response="200", description="OK"),
+     *     @OA\Response(response="422", description="Validation Exception"),
+     *     @OA\Response(response="500", description="Fail"),
+     * )
+     */
+    public function show(Request $request)
     {
-        return response()->json(['reservations' => SalonReservation::all()]);
+        try {
+            $validated = $request->validate([
+                'status' => 'size:1',
+                'start_date' => 'date',
+                'end_date' => 'date',
+            ]);
+        } catch (ValidationException $validationException) {
+            $errorStatus = $validationException->status;
+            $errorMessage = $validationException->getMessage();
+            return response()->json(['error' => $errorMessage], $errorStatus);
+        }
+
+        if(isset($validated['status'])) {
+            $reservations = SalonReservation::where('status', $validated['status'])->get();
+        }
+        else {
+            $reservations = SalonReservation::all();
+        }
+
+        if(isset($validated['start_date'])) {
+            $start_date = date('Y-m-d 00:00:00', strtotime($validated['start_date']));
+            $reservations = $reservations->where('reservation_date', '>=', $start_date);
+        }
+
+        if(isset($validated['end_date'])) {
+            $end_date = date('Y-m-d 23:59:59', strtotime($validated['end_date']));
+            $reservations = $reservations->where('reservation_date', '<=', $end_date);
+        }
+
+        return response()->json(['reservations' => $reservations]);
     }
 
+    /**
+     * @OA\Patch (
+     *     path="/api/admin/salon-reservation",
+     *     tags={"미용실"},
+     *     summary="예약 정보 업데이트(관리자)",
+     *     description="미용실 예약 상태를 업데이트",
+     *     @OA\RequestBody(
+     *         description="업데이트할 상태 및 아이디",
+     *         required=true,
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema (
+     *                 @OA\Property (property="id", type="integer", description="예약 아이디", example=1),
+     *                 @OA\Property (property="status", type="char", description="예약의 상태(C,R 중 하나로 보내면 됨, 각각 Confirm, Reject 임)", example="R"),
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(response="200", description="OK"),
+     *     @OA\Response(response="422", description="Validation Exception"),
+     *     @OA\Response(response="500", description="Fail"),
+     * )
+     */
     public function update(Request $request)
     {
         try {
