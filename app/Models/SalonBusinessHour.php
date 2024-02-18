f697bb8601013040459459d338d2cae110e5ee91
Hyn2 <justin010129@gmail.com>
2024-02-18 19:09:19 +0900

    Migrate: 美容室の時間関連テーブル、モデル生成

---
+++ app/Models/SalonBusinessHour.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SalonBusinessHour extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        's_time',
+        'e_time'
+    ];
+}
