fd43c41a2dac1976453e4bb4f3f557d3fec6abdd
jihun <tohka113@naver.com>
2024-02-20 14:11:02 +0900

    Feat:学校のrestaurant_menu Excel file import

--- app/Imports/RestaurantMenuImport.php
+++ app/Imports/RestaurantMenuImport.php
@@ -4,21 +4,60 @@ namespace App\Imports;
 
 use App\Models\RestaurantMenu;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Log;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Carbon\Carbon;
 
-class RestaurantMenuImport implements ToModel
+
+
+class RestaurantMenuImport implements ToCollection
 {
     /**
     * @param Collection $collection
     */
-    public function model (array $row)
-    {
-        
-        return new RestaurantMenu([
-            'date' => date('Y-m-d', strtotime($row[0])), // 날짜 값 파싱
-            'meal_type'           => $row[1],
-            'menu'                => $row[2],
-        ]);
+    public function collection (Collection $rows)
+    {       
+        for($j = 1; $j < 6; $j++){
+            $i = 5;
+            $menu= "";
+            for ($i ; $i < 25; $i++){
+                if($i === 5) {
+                    $date = Carbon::createFromTimestamp(($rows[$i][$j] - 25569) * 86400);
+                }
+                if($i === 6) {
+                    $menu= "";
+                    continue;
+                }
+                $menu = $menu . " " . $rows[$i][$j];
+                
+                if ($i % 6 === 0 ) {
+                    switch($i){
+                        case 12:
+                            $meal_type = 'b';
+                            break;
+                        case 18:
+                            $meal_type = 'l';
+                            break;
+                        case 24:
+                            $meal_type = 'd';
+                            break;
+                        default:
+                            $meal_type = "error";
+                    }
+
+                    $menuData =new RestaurantMenu([
+                        'date' => $date,
+                        'menu'   => $menu,
+                        'meal_type' => $meal_type,
+                    ]);
+                    $menuData->save();
+                    $menu = "";
+                }
+                
+            }
+        }
+        return;
     }
+  
 }
