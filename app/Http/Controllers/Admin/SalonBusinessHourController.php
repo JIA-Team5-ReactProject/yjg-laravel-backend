6854f36c490bd3b0d2e0a555dbe7fd2a86bfc5f2
Hyn2 <justin010129@gmail.com>
2024-02-20 21:12:43 +0900

    Feat: 美容室営業時間関連機能実装

--- app/Http/Controllers/Admin/SalonBusinessHourController.php
+++ app/Http/Controllers/Admin/SalonBusinessHourController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\SalonBusinessHour;
+use DateInterval;
+use DateTime;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
@@ -13,7 +15,83 @@ use Illuminate\Validation\ValidationException;
 class SalonBusinessHourController extends Controller
 {
     //TODO: 휴게시간 필터링하여 불러오는 기능 구현
-    private $dateRule = ['MON, TUE, WEN, THU, FRI, SAT, SUN'];
+
+    public function __construct(protected  SalonBreakTimeController $salonBreakTimeController)
+    {
+    }
+    /**
+     * @OA\Get (
+     *     path="/api/admin/salon-hour",
+     *     tags={"미용실"},
+     *     summary="전체 영업시간",
+     *     description="모든 요일의 미용실 영업 시간",
+     *     @OA\Response(response="200", description="Success"),
+     *     @OA\Response(response="422", description="Validation Error"),
+     * )
+     */
+    public function index(Request $request)
+    {
+        return response()->json(['business_hours' => SalonBusinessHour::all(['s_time', 'e_time', 'date'])]);
+    }
+
+    /**
+     * @OA\Get (
+     *     path="/api/admin/salon-hour/{day}",
+     *     tags={"미용실"},
+     *     summary="특정 요일의 영업시간",
+     *     description="특정 요일의 미용실 영업 시간을 한시간 단위로 반환함",
+     *      @OA\Parameter(
+     *            name="day",
+     *            description="요일(MON,TUE... 이런식으로)",
+     *            required=true,
+     *            in="path",
+     *            @OA\Schema(type="string"),
+     *        ),
+     *     @OA\Response(response="200", description="Success"),
+     *     @OA\Response(response="422", description="Validation Error"),
+     * )
+     */
+    public function show(string $day)
+    {
+        $validator = Validator::make(['day' => $day], [
+            'day' => ['required', Rule::in($this->dayList)],
+        ]);
+
+        try {
+            $validated = $validator->validate();
+        } catch (ValidationException $validationException) {
+            $errorStatus = $validationException->status;
+            $errorMessage = $validationException->getMessage();
+            return response()->json(['error' => $errorMessage], $errorStatus);
+        }
+
+        $breakTimes = [];
+        $salonBreakTimes = $this->salonBreakTimeController->index()->where('day', $day);
+
+        foreach($salonBreakTimes as $salonBreakTime) {
+            $breakTimes[] = $salonBreakTime->break_time;
+        }
+
+        $b_hour = SalonBusinessHour::where('date' , $day)->first();
+
+        // UNIX 타임스탬프로 변경
+        $current = strtotime($b_hour->s_time);
+        $end = strtotime($b_hour->e_time);
+        $business_hours = [];
+
+        while ($current <= $end) {
+            $business_hours[] = (object) ['time' => date('H:i', $current), 'available' => true];
+            $current = strtotime('+1 hour', $current);
+        }
+
+        foreach ($business_hours as $business_hour) {
+            if(in_array($business_hour->time, $breakTimes)) {
+                $business_hour->available = false;
+            }
+        }
+
+        return $business_hours;
+    }
 
     /**
      * @OA\Post (
