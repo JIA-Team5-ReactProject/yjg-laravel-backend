7ae260a71d1dd0321c4d5c56c4c13c142492c838
Hyn2 <justin010129@gmail.com>
2024-02-21 16:25:16 +0900

    Feat: 美容室の営業時間Seeder実装

---
+++ database/seeders/SalonBusinessHourSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\SalonBusinessHour;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SalonBusinessHourSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $dayList = ['MON', 'TUE', 'WEN', 'THU', 'FRI', 'SAT', 'SUN'];
+
+        foreach ($dayList as $day) {
+            SalonBusinessHour::create([
+                's_time' => date('H:i', strtotime('09:00')),
+                'e_time' => date('H:i', strtotime('18:00')),
+                'date'   => $day,
+            ]);
+        }
+    }
+}
