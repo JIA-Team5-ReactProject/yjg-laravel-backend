4c7695d68254f42065e479f8aee8d2bfe5e6c2c5
Hyn2 <justin010129@gmail.com>
2024-02-29 14:23:53 +0900

    Feat: 認証関連のmiddlewareを実装

---
+++ app/Http/Middleware/ApproveCheck.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApproveCheck
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        try {
+            $user = User::where('email', $request->email)->firstOrFail();
+        } catch (ModelNotFoundException $modelException) {
+            return response()->json(['error' => '해당하는 유저가 없습니다.'], 404);
+        }
+
+        if(!$user->approved) {
+            return response()->json(['error' => '아직 승인되지 않은 유저입니다.'], 500);
+        }
+
+        return $next($request);
+    }
+}
