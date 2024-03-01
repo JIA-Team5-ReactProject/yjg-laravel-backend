fd76f02f2a56bbec4d73f8b3313c910a523b0a0e
Hyn2 <justin010129@gmail.com>
2024-03-02 01:55:41 +0900

    Feat: ASのseederとfactoryを実装(漏れたファイル)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             MeetingRoomReservationSeeder::class,
             NoticeSeeder::class,
+            AfterServiceSeeder::class,
         ]);
     }
 }
