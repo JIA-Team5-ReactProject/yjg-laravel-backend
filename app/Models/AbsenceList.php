073faa21bcc20de5a001a00c58c8d71490e1ef91
Hyn2 <justin010129@gmail.com>
2024-03-05 16:09:45 +0900

    Rename: モデル名を修正

--- app/Models/AbsenceList.php
+++ app/Models/AbsenceList.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Absence extends Model
+class AbsenceList extends Model
 {
     use HasFactory;
     protected $fillable = [
+        'user_id',
         'start_date',
         'end_date',
         'type',
