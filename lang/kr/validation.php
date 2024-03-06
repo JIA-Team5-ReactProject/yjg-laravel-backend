b01513157880a330d6087c5d543e01d1f623697c
Hyn2 <justin010129@gmail.com>
2024-03-06 18:51:23 +0900

    Refactor: Validationメッセージを修正

--- lang/kr/validation.php
+++ lang/kr/validation.php
@@ -13,6 +13,7 @@ return [
         'admin_privilege' => '행정 권한',
         'restaurant_privilege' => '식당 권한',
         'approve' => '승인 여부',
+        'new_password' => '새로운 비밀번호',
     ],
     // Type
     'numeric' => ':attribute 은(는) 숫자이어야 합니다.',
@@ -23,11 +24,13 @@ return [
     // rule
     'unique' => '이미 사용 중인 :attribute 입니다.',
     'required' => ':attribute 를 입력해주세요.',
+    'required_with' => ':attribute 를 입력해주세요.',
     'email' => ':attribute 형식을 확인하세요.',
     'in' => ':attribute 의 값은 허용되지 않는 값입니다.',
     'date_format' => ':attribute 값이 날짜(시간) 형식과 일치하지 않습니다.',
     'date' => ':attribute 의 값은 날짜 형식이어야 합니다.',
     'exists' => '해당하는 :attribute 이(가) 존재하지 않습니다.',
+    'current_password' => '기존 비밀번호가 일치하지 않습니다.',
 
 
 
