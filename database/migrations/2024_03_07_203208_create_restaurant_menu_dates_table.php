c0bbe702a634ee00b123e205f72ee91d5a1a85db
jihun <tohka113@naver.com>
2024-03-19 10:07:46 +0900

    fix:auth user_id 修正

--- database/migrations/2024_03_07_203208_create_restaurant_menu_dates_table.php
+++ database/migrations/2024_03_07_203208_create_restaurant_menu_dates_table.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\RestaurantMenuDate;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -18,6 +20,26 @@ return new class extends Migration
             $table->string('week');
             $table->timestamps();
         });
+        $yearUp = ((int) date('Y'));
+        $years = [date('Y'), (string)$yearUp+1,(string)$yearUp+2,(string)$yearUp+3,(string)$yearUp+4,];
+        
+
+        $months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+        $weeks = ['1', '2', '3', '4', '5'];
+
+        foreach ($years as $year) {
+            foreach ($months as $month) {
+                foreach ($weeks as $week) {
+                    DB::table('restaurant_menu_dates')->insert([
+                        'year' => $year,
+                        'month' => $month,
+                        'week' => $week,
+                        'created_at' => now(),
+                        'updated_at' => now(),
+                    ]);
+                }
+            }
+        }
     }
 
     /**
