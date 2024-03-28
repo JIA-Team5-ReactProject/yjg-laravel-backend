c9a07d67986a2cbc21ee2fa4ea6f2a1dd4c7272a
Hyn2 <justin010129@gmail.com>
2024-03-28 09:23:37 +0900

    Refactor: ユーザーモデルを修正

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,6 +13,8 @@ class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    protected $with = ['privileges:id,privilege'];
+
     /**
      * Get the identifier that will be stored in the subject claim of the JWT.
      *
