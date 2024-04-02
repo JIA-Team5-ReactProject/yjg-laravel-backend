512a49113c8222e1552434db482c02b2001adefb
Hyn2 <justin010129@gmail.com>
2024-04-02 21:44:28 +0900

    Fix: noticeの作成関係エラーを解決

--- app/Http/Controllers/Administrator/NoticeController.php
+++ app/Http/Controllers/Administrator/NoticeController.php
@@ -208,7 +208,7 @@ class NoticeController extends Controller
             return response()->json(['error'=>$errorMessage], $errorStatus);
         }
 
-        $validated['admin_id'] = auth()->id();
+        $validated['user_id'] = auth()->id();
 
         // 불필요하다고 판단되어 모델에서 현재 인증된 사용자를 찾는 로직은 삭제함
 
